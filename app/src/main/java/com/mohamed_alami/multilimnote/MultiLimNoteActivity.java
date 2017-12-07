b64b3042ca5c032e9f44fe986cccc724c0c070b7
Mohamed Alami <alami-chentou1>
2017-12-07 20:34:32 +0100

    Début du projet

---
+++ app/src/main/java/com/mohamed_alami/multilimnote/MultiLimNoteActivity.java
@@ -0,0 +1,13 @@
+package com.mohamed_alami.multilimnote;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MultiLimNoteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_multi_lim_note);
+    }
+}
